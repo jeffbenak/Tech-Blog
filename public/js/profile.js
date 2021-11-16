0c8048337154264dd93715fa3d33b9e923cea1af
Jeff Benak <jeff.benak@yahoo.com>
2021-11-16 15:20:21 -0600

    Changed handlebars to reflect back end changes, changed all instances of name to title, and all description instances to body

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,14 +1,13 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#post-name').value.trim();
-  const needed_funding = document.querySelector('#post-funding').value.trim();
-  const description = document.querySelector('#post-desc').value.trim();
+  const title = document.querySelector('#post-title').value.trim();
+  const body = document.querySelector('#post-body').value.trim();
 
-  if (name && needed_funding && description) {
+  if (title && body) {
     const response = await fetch(`/api/posts`, {
       method: 'POST',
-      body: JSON.stringify({ name, needed_funding, description }),
+      body: JSON.stringify({ title, body }),
       headers: {
         'Content-Type': 'application/json',
       },
